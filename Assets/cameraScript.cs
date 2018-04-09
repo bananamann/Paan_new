82bf42dca974185b3ea91e8e82010f228ef6f2bf
Pierce <test@test.com>
2018-04-08 20:15:18 -0400

    removed some extra commented-out code

--- Assets/cameraScript.cs
+++ Assets/cameraScript.cs
@@ -16,39 +16,9 @@ public class cameraScript : MonoBehaviour {
         player = GameObject.FindGameObjectWithTag("player");
 	}
 
-    void Update() {
-        //if (player.transform.position.x < 0f) {
-        //    followX = false;
-        //} else {
-        //    followX = true;
-        //}
-
-        //if (player.transform.position.y < 0) {
-        //    followY = false;
-        //} else {
-        //    followY = true;
-        //}
-    }
-
 	void FixedUpdate () {
-        //float xPos = transform.position.x;
-        //float yPos = transform.position.y;
-
-        //if (followX) {
-        //    xPos = player.transform.position.x;
-        //} else {
-        //    xPos = transform.position.x;
-        //}
-
-        //if (followY) {
-        //    yPos = player.transform.position.y;
-        //} else {
-        //    yPos = transform.position.y;
-        //}
-
         Vector3 desiredPosition = (new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z)) + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-        //transform.position = new Vector3(xPos, yPos, transform.position.z);
     }
 }
